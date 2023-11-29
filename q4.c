f5bf0718c93e9b27a49d8ae8f95beff299500b45
heenasavaliya <heenasavaliya331@gmail.com>
2023-11-29 17:41:03 +0530

    heena

--- q4.c
+++ q4.c
@@ -2,15 +2,15 @@
 int main(){
 
  int age;
- age=
+ 
  
  
 printf("Enter your age : ");
 scanf("%d",&age);
 
-if(age < -18){
+if(age > 18){
     printf("you can vote");
-}else if(age < -18){
+}else if(age < 18){
     printf("you cannot vote");
 }else{
     printf("you can also vote");
