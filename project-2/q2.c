2c50648dcc44c92d969e741f0f2f1cab4f0048b5
heenasavaliya <heenasavaliya331@gmail.com>
2023-12-04 15:54:35 +0530

    done

---
+++ project-2/q2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+
+    int a,b,c,d;
+
+
+    printf("First 50 unit rs.1unit : ");
+    scanf("%d",a);
+
+    printf("second 100 unit rs.2unit : ");
+    scanf("%d",b);
+
+
+    printf("third 100 unit rs.3unit : ");
+    scanf("%d",c);
+
+    printf("forth 250 unit rs.4unit : ");
+    scanf("%d",d);
+
+
+
+}
