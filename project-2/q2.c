b24fefa933369982503cb27ec28f5217353105f2
heenasavaliya <heenasavaliya331@gmail.com>
2023-12-05 17:35:10 +0530

    done

--- project-2/q2.c
+++ project-2/q2.c
@@ -1,22 +1,45 @@
-#include<stdio.h>
-int main(){
+/**
+ * C program to calculate total electricity bill
+ */
 
-    int a,b,c,d;
+#include <stdio.h>
 
+int main()
+{
+    int unit;
+    float amt, total_amt, sur_charge;
 
-    printf("First 50 unit rs.1unit : ");
-    scanf("%d",a);
-
-    printf("second 100 unit rs.2unit : ");
-    scanf("%d",b);
-
-
-    printf("third 100 unit rs.3unit : ");
-    scanf("%d",c);
-
-    printf("forth 250 unit rs.4unit : ");
-    scanf("%d",d);
+    /* Input unit consumed from user */
+    printf("Enter total units consumed: ");
+    scanf("%d", &unit);
 
 
+    /* Calculate electricity bill according to given conditions */
+    if(unit <= 50)
+    {
+        amt = unit * 1;
+    }
+    else if(unit <= 150)
+    {
+        amt = 50 + ((unit-50) * 2);
+    }
+    else if(unit <= 250)
+    {
+        amt = 100 + ((unit-150) * 1.20);
+    }
+    else
+    {
+        amt = 220 + ((unit-250) * 1.50);
+    }
 
+    /*
+     * Calculate total electricity bill
+     * after adding surcharge
+     */
+    sur_charge = amt * 0.20;
+    total_amt  = amt + sur_charge;
+
+    printf("Electricity Bill = Rs. %.2f", total_amt);
+
+    return 0;
 }
